aeb40dbe4a8db28cc24526c58519301df4c881cf
ahmed <ahmedkhaled8901@gmail.com>
2022-02-03 20:37:54 +0200

    fix updateUI function

--- website/app.js
+++ website/app.js
@@ -1,20 +1,38 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey = '&appid=ae8dc7d1b918f20a8becea7657780e34';
+const url = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+//Add Event Listener to the Generate Button
+document.getElementById('generate').addEventListener('click', doSomthing);
+function doSomthing(event){
+    const zip = document.getElementById('zip').value;
+    console.log(zip);
+    const comment = document.getElementById('feelings').value;
+    console.log(comment)
+    getData(url,apiKey,zip).then(data =>{
+        postData('/addWeatherData', {date: newDate, temp:data, content:comment})
+    }).then(
+        updateUI()
+    )
+
+}
 /* Function to GET Web API Data*/
 const getData = async (url, apiKey, zip) => {
     const apiLink = url + zip + ',us' +apiKey;
     console.log(apiLink)
-    const response = await (await fetch(apiLink)).json();
+    const response = await fetch(apiLink);
     console.log(response);
     try {
-        const newdata = response;
+        const newdata = await response.json();
         console.log(newdata);
         console.log(newdata.main.temp);
+        return newdata.main.temp;
 
     }catch(error){
         console.log("error", error);
@@ -63,5 +81,5 @@ const updateUI = async () => {
 }
 
 //getData('https://api.openweathermap.org/data/2.5/weather?zip=', '&appid=ae8dc7d1b918f20a8becea7657780e34', '94040')
-postData('/addWeatherData', {date: newDate, temp:'5264waadsfs', content:"hahahah"})
-updateUI()
+//postData('/addWeatherData', {date: newDate, temp:'5264waadsfs', content:"hahahah"})
+//updateUI()
