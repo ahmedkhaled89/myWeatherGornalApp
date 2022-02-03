853c565f54ecb759dce8c74e4306ab82d711029e
ahmed <ahmedkhaled8901@gmail.com>
2022-02-03 14:58:54 +0200

    Edite getData(), add await for .json().

--- website/app.js
+++ website/app.js
@@ -7,9 +7,13 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 /* Function to GET Web API Data*/
 const getData = async (url, apiKey, zip) => {
     const apiLink = url + zip + ',us' +apiKey;
-    const response = await fetch(apiLink);
+    console.log(apiLink)
+    const response = await (await fetch(apiLink)).json();
+    console.log(response);
     try {
-        const temp = response.json().main.temp;
+        const newdata = response;
+        console.log(newdata);
+        console.log(newdata.main.temp);
 
     }catch(error){
         console.log("error", error);
@@ -18,3 +22,4 @@ const getData = async (url, apiKey, zip) => {
 
 
 }
+getData('https://api.openweathermap.org/data/2.5/weather?zip=', '&appid=ae8dc7d1b918f20a8becea7657780e34', '94040')
