5ee965b57865fdc967206d31ba3d24ba5fb04d6a
ahmed <ahmedkhaled8901@gmail.com>
2022-02-03 19:20:46 +0200

    add postData Function

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
 
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
@@ -22,4 +23,30 @@ const getData = async (url, apiKey, zip) => {
 
 
 }
-getData('https://api.openweathermap.org/data/2.5/weather?zip=', '&appid=ae8dc7d1b918f20a8becea7657780e34', '94040')
+
+/* Function to POST data */
+const postData = async (url ='', data = {}) => {
+    console.log(data);
+    const response = await fetch(url,{
+        method : "POST",
+        credentials: 'same-origin',
+        headers: {
+        
+            'Content-Type': 'application/json',
+        },
+        // Body data type must match "Content-Type" header        
+        body: JSON.stringify(data), 
+    });
+    try{
+        const newDate = await response.json();
+        console.log(newDate);
+        return newDate;
+    }catch(error){
+        console.log("error", error);
+
+    }
+
+}
+
+//getData('https://api.openweathermap.org/data/2.5/weather?zip=', '&appid=ae8dc7d1b918f20a8becea7657780e34', '94040')
+postData('/addWeatherData', {temp:'5264waadsfs'})
