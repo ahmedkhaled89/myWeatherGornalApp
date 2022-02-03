5f91df7b2051e04145e369f83ae9fb8e7c6c29cc
ahmed <ahmedkhaled8901@gmail.com>
2022-02-03 18:05:25 +0200

    Setup the server

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
+const port = 8000;
 
-// Require Express to run server and routes
+// Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
+/* Dependencies */
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log(`WJA Running On Port ${port}`)
+}
