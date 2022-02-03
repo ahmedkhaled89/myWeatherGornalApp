f51270d1adcd04c55f9537278e459a5b0c254a6f
ahmed <ahmedkhaled8901@gmail.com>
2022-02-03 11:30:35 +0200

    Add getData Function to get Web Api Data to app.js file

--- website/app.js
+++ website/app.js
@@ -3,3 +3,18 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+/* Function to GET Web API Data*/
+const getData = async (url, apiKey, zip) => {
+    const apiLink = url + zip + ',us' +apiKey;
+    const response = await fetch(apiLink);
+    try {
+        const temp = response.json().main.temp;
+
+    }catch(error){
+        console.log("error", error);
+
+    }
+
+
+}
