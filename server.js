d174a06900beda1c0b2f5bd780638b8d0f259f63
ahmed <ahmedkhaled8901@gmail.com>
2022-02-04 15:05:29 +0200

    replace bodyParser by express

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const express = require('express');
 const app = express();
 
 //body barser
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+//const bodyParser = require('body-parser');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 /* Middleware*/
